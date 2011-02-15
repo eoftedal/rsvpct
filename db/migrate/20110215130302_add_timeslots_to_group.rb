b12484e4a324e926cd73ec826f88dbdd464359e4
Erlend Oftedal <erlend@oftedal.no>
2011-02-15 15:38:03 +0100

    fixing migration

--- db/migrate/20110215130302_add_timeslots_to_group.rb
+++ db/migrate/20110215130302_add_timeslots_to_group.rb
@@ -1,6 +1,6 @@
 class AddTimeslotsToGroup < ActiveRecord::Migration
   def self.up
-	change_table :timeslot do |t|
+	change_table :timeslots do |t|
 		t.references :group, :default => 1
 	 end
   end
