276913cd3ced3ba9614d5c527a3bdc04d42d86a0
Erlend Oftedal <erlend@oftedal.no>
2011-02-15 15:33:17 +0100

    adding groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
 	has_many :timeslots
-	has_many :memberships, :dependent => true
+	has_many :memberships
 	has_many :players, :through => :memberships
 	
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
 	validates_presence_of :email#, :oauth_token, :oauth_secret
 	validates_uniqueness_of :email
-	has_many :memberships, :dependent => true
+	has_many :memberships
 	has_many :groups, :through => :memberships
 end
