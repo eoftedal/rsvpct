52a19b4ca9eab5744829339c51715c4c3ae9a40e
Erlend Oftedal <erlend@oftedal.no>
2011-03-08 20:19:50 +0100

    moving MD5 def

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,12 @@
 module ApplicationHelper
+	require 'digest/md5'
 	def dateFormat (date) 
 		date.strftime "%Y-%m-%d %H:%M"
 	end
 	def current_group 
 		Group.find(params[:group_id])
+	end
+	def md5 (text) 
+		Digest::MD5.hexdigest(text)
 	end
 end
