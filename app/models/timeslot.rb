11d7f40d04597fe031ed245991b6b1c65efa4446
Erlend Oftedal <erlend@oftedal.no>
2011-03-08 20:24:10 +0100

    UI updates

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,13 +1,11 @@
 class Timeslot < ActiveRecord::Base
 	has_many :answers
 	belongs_to :group
-	
-	require 'digest/md5'
-	
-	
+
 	def coming
 		answers.select { |a| a.joining }
 	end
+	
 	def coming_count 
 		coming().map{|a| a.extra + 1 }.reduce(0) { |sum, value | sum + value } 
 	end
@@ -15,11 +13,9 @@ class Timeslot < ActiveRecord::Base
 	def not_coming
 		answers.select { |a| !a.joining }
 	end
+	
 	def no_answer
 		players = Player.find(:all)
 		players.select { |p| answers.detect { |a| a.player == p } == NIL }
-	end
-	def md5 (text) 
-		Digest::MD5.hexdigest(text)
 	end
 end
